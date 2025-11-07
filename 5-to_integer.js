3b572fdb91cb4519a4f5288c478d14cc62836be8
Otega Otite <otiteotega@gmail.com>
2025-11-07 15:08:09 +0100

    An integer

---
+++ 5-to_integer.js
@@ -0,0 +1,14 @@
+/**
+ * Write a script that prints My number: <first argument converted in integer> if
+ * the first argument can be converted to an integer:
+ * 
+ * If the argument can't be converted to an integer, print "Not a number"
+ */
+
+
+const firstArg = process.argv[2]
+if (firstArg !== undefined && !isNaN(parseInt(firstArg))) {
+    console.log("My number: ", firstArg)
+} else {
+    console.log("Not a Number")
+}
