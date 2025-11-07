c8450d134f6fbbcea62fb002f161b362d4f0d213
Otega Otite <otiteotega@gmail.com>
2025-11-07 15:21:25 +0100

    loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+/**
+ * Write a script that prints 3 lines: but by using an array of string and a loop
+ * 
+ * The first line is "C is fun"
+ * the second line: "Python is cool"
+ * The third line: "JavaScript is amazing"
+ */
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const print of myVar) {
+    console.log(print)
+}
